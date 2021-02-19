e77c97994199b90e89a41b423e82a2b17e91ef38
asadalibest1 <asadalibest1@gmail.com>
2021-02-19 11:43:07 +0500

    commit

--- src/shoe-store-app/components/copyRight.js
+++
@@ -1,8 +0,0 @@
-import React from'react';
-import "./css/Copyright.css"
-
-export default function Copyright(){
-    return(
-         <p className="copyRight">Copyright © 2020. All Rights Reserved</p>  
-    )
-}
